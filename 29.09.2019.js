2df1bbb92591212f18f61028859154ad8984480e
SyedZeeshanHussain <47963919+SyedZeeshanHussain@users.noreply.github.com>
2019-09-29 21:33:10 +0500

    Add files via upload

---
+++ 29.09.2019.js
@@ -0,0 +1,232 @@
+/*
+var number = 5;
+number = number + 1; //6
+
+number += 1; //7
+number++; // 8
+alert(number);
+
+var number = 5;
+++number;
+alert(number); //6 */
+
+/*
+var number = 5;
+num = number++;
+alert(num); //5 */
+
+/*
+var number = 5;
+ //number = number + 5;
+ var n = number++;
+ alert("number = " + number + "  n = " + n);
+
+ var number = 5;
+ var n = ++number;
+ alert("number = " + number + "  n = " + n);
+
+ var number = 12;
+ var n = number++;
+ alert("number = " + number + "  n = " + n);
+
+ var number = 12;
+ var n = number--;
+ alert("number = " + number + "  n = " + n);
+ */
+/*
+var number = 12;
+var c = number++ - --number + ++number;
+alert("number = " + number + "  c = " + c);
+
+var number = 10; // 8
+var c = --number + number-- - ++number - --number;
+// var c = 9 + 9 - 9 - 8 = 1
+alert("number = " + number + "  c = " + c);
+*/
+
+// if Statement 
+
+/*
+var a = prompt("Are you blockchain student?");
+if (a == "yes") {
+   alert("Welcome in class");
+}
+else alert("Get lost"); */
+
+/*
+var a = prompt("Are you blockchain student?");
+if (a == "yes") {
+   alert("Welcome in class");
+}
+else if (a == "Yes") {
+    alert("Welcome in Class");
+}
+else alert("Get lost"); */
+
+/*
+var a = prompt("Are you blockchain student?");
+if (a != "yes") {
+   alert("Welcome in class");
+}
+else alert("Get lost"); 
+*/
+
+/*
+var a = prompt("What is your %?");
+if(a > 80) {
+    alert("Grade A+");
+}
+else if (a > 70) {
+    alert("Grade A");
+}
+else if (a > 60) {
+   alert("Grade B");
+}
+else alert("Fail");
+*/
+
+/*
+var a = prompt("What is your %?");
+ if(a >= 80) {
+     alert("Grade A+");
+ }
+ else if (a >= 70) {
+     alert("Grade A");
+ }
+ else if (a >= 60) {
+    alert("Grade B");
+}
+else alert("Fail"); */
+
+/*
+var a = prompt("What is your %?");
+ if(a >= 80 && a <= 100) {
+     alert("Grade A+");
+ }
+ else if (a >= 70) {
+     alert("Grade A");
+ }
+ else if (a >= 60) {
+    alert("Grade B");
+}
+else alert("Fail"); */
+
+
+/*
+var a = prompt("What is your %?");
+// && Left and Right should be True
+// || Left or Right anyone should be True
+ if(a > 60 && a <= 70) {
+     alert("Grade B");
+ }
+ else if (a > 70 && a <= 80) {
+     alert("Grade A");
+ }
+ else if (a > 80 && a <= 100) {
+    alert("Grade A+");
+}
+else alert("Fail");
+*/
+// ----------------------------------- ///
+// Class Assignment (Make Calculator).
+
+/*
+var a = 5;
+var b = 7;
+var c = prompt("Enter operator");
+if (c == "+") {
+    alert(a+b);
+}
+else if (c == "-") {
+    alert(a-b);
+}
+else if (c == "*") {
+    alert(a*b)
+}
+else if (c == "/") {
+    alert(a/b);
+}
+else alert("Enter correct operator");
+*/
+
+/*
+var a = prompt("Enter Value 1");
+var b = prompt("Enter Value 2");
+var c = prompt("Enter operator");
+var aa = parseInt(a);
+var bb = parseInt(b);
+if (c == "+") {
+    alert(aa+bb);
+}
+else if (c == "-") {
+    alert(aa-bb);
+}
+else if (c == "*") {
+    alert(aa*bb)
+}
+else if (c == "/") {
+    alert(aa/bb);
+}
+else alert("Enter correct operator");
+*/
+// ----------------------------------- //
+
+/*
+var a = 50;
+if (a == "50") {
+    alert("Yes 50");
+}
+else ("Record Not Found");
+
+var a = 50;
+if (a === "50") {
+    alert("Yes 50");
+}
+else alert("Record Not Found");
+*/
+
+/*
+var a = prompt("Enter Character");
+if (a == "A" || a == "E" || a == "I" || a == "O" || a == "U") {
+    alert("It is Vowel");
+}
+else alert("It is not Vowel");
+*/
+
+
+/*
+var score = prompt("Enter your percentage");
+if (score >= 80 && score <= 100) {
+    alert("A+ Grade");
+}
+else if (score >= 70 && score < 80) {
+    alert("A Grade");
+} 
+else if (score >= 60 && score < 70) {
+    alert("B Grade");
+}  
+else if (score >= 50 && score < 60) {
+    alert("C Grade");
+}
+else alert("Fail");
+
+*/
+
+// if Nested Satements 
+var score = prompt("Enter your percentage");
+if (score >= 80 && score <= 100) {
+    alert("A+ Grade");
+    if (score >= 80 && score <= 100) {
+        alert("Award 10,000");
+    }
+}
+else if (score >= 70 && score < 80) {
+    alert("A Grade");
+}
+else if (score >= 60 && score < 70) {
+    alert("B Grade");
+}
+else if (score >= 50 && score < 60) {
+    alert("C Grade");
+}
+else alert("Fail");
